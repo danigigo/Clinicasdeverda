58e44bafe473b602a1fa3bc312b934b012be15aa
danigigo <camachocavo2001@hotmail.com>
2024-05-22 19:54:16 -0500

    d

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const confirmPassword = document.getElementById('confirm-password').value;
-        const celular = document.getElementById('celular').value;
+        const Celular = document.getElementById('celular').value;
         const role = document.getElementById('role').value;
 
         if (password !== confirmPassword) {
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        const data = { nombre, email, password, celular };
+        const data = { nombre, email, password, Celular };
 
         try {
             let response;
